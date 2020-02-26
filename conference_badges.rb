2d2a5afef65823c8a35ec22b7b17770294175c41
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-26 19:21:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def badge_maker(name)
-array = ["Arel"]
+array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 array.each do |name|
   puts "Hello, my name is #{name}"
   end
