a6a16411ef0b524c3b3be8eeeccef1c37fbf44a6
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-26 19:30:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,31 +4,3 @@ def badge_maker(name)
   badge
 end
 
-def batch_badge_creator(name)
-  badge_list = [] 
-  attendees.map { |badges| badge_maker(badges) }
-  name.each do |badges|	
-    badge_maker(badges)	
-  end	
-  badge_list << badges	
-end	
-
-
-	def assign_rooms(name)
-  rooms = []
-
-  name.each.with_index(1) do |name, room_assignment|
-    rooms << room_assignments = "Hello, #{name}! You'll be assigned to room #{room_assignment}!"
-  end
-  rooms
-
-end
-
-def printer(name)
-  batch_badge_creator(name).each do |badges|
-    puts badges
-  end
-  assign_rooms(name).each do |room_assignment|
-    puts room_assignment
-  end
-end	
