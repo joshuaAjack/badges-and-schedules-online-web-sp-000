b11b4a02aaf904aa6d54c58034cf051e39ed6b72
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-26 19:50:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ def assign_rooms(atendees)
   new_array = []
   counter = 1 
   atendees.each do |name|
-    new_array. push("Hello, #{name}! You'll be assigned to room #{new_array}")
+    new_array. push("Hello, #{name}! You'll be assigned to room #{counter}")
     counter += 1
   end
   return new_array
